efd9469b613e46002f0df24d0920f389c477c0f4
catherine <cathttqq@hotmail.ca>
2018-09-10 11:58:59 -0700

    adding a new feature

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+//adding some comments for dummy check in
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
